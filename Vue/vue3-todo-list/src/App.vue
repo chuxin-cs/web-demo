5e5cd37e8d317e3b24355b1b47c559f26454d493
chuxin <15115359055@139.com>
2025-03-28 15:57:59 +0800

    feat: 添加基础版TodoList组件及相关类型定义
    新增了基础版的TodoList组件，包括TodoInput、List和类型定义文件，并更新了主应用以展示该组件

--- Vue/vue3-todo-list/src/App.vue
+++ Vue/vue3-todo-list/src/App.vue
@@ -1,8 +1,12 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <div>
-    
+  <div class="app-container">
+    <!-- 基础版本 -->
+    <h1>基础版本</h1>
+    <V1TodoList />
+    <hr>
   </div>
 </template>
+
+<script setup lang="ts">
+import {V1TodoList} from "./TodoList"
+</script>
---
+++ Vue/vue3-todo-list/src/TodoList/index.ts
@@ -0,0 +1,5 @@
+import V1TodoList from './v1/index.vue'
+
+export {
+  V1TodoList
+}
