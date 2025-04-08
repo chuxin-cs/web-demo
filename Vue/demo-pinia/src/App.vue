cf37a808573cc07c2f0ef7d3cd031b8598168191
chuxin <15115359055@139.com>
2025-04-08 15:41:20 +0800

    feat: 添加Pinia状态管理并配置Vite别名
    在项目中引入Pinia作为状态管理工具，并配置Vite的路径别名以便于模块引用。新增了Todo列表功能，支持添加和删除任务。同时，更新了相关依赖以支持新功能。

--- Vue/demo-pinia/src/App.vue
+++ Vue/demo-pinia/src/App.vue
@@ -1,6 +1,35 @@
-
 <template>
-  <div class="app-container"></div>
+  <div class="app-container">
+    <input v-model="newTodo" placeholder="添加新任务" @keyup.enter="addTodo" />
+    <button @click="handeAddTodo">添加</button>
+    <ul>
+      <li v-for="(todo, index) in store.todos" :key="index">
+        <span
+          :style="{ textDecoration: todo.completed ? 'line-through' : 'none' }"
+        >
+          {{ todo.text }}
+        </span>
+        <button @click="removeTodo(todo.id)">删除</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useTodoStore } from '@/store';
+
+const newTodo = ref("")
+const store = useTodoStore();
+const handeAddTodo = () => {
+  if (newTodo.value.trim()) {
+    store.addTodo(newTodo.value);
+    newTodo.value = '';
+  }
+};
+
+const removeTodo = (id: number) => {
+  store.deleteTodo(id);
+};
+
+</script>
