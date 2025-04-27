1b99a3eeb09f33fcc658a398f748cec627c27303
chuxin <15115359055@139.com>
2025-04-27 17:12:06 +0800

    feat(home): 添加主页布局和样式
    新增了主页的布局组件和样式文件，使用 antd 实现导航菜单，并调整了页面结构和样式以提升用户体验

--- react-hooks/src/pages/home/index.tsx
+++ react-hooks/src/pages/home/index.tsx
@@ -1,10 +1,20 @@
 import { Outlet } from "react-router-dom";
+import Layouts from "./layout";
+import { Layout } from "antd";
+import "./index.less"
 
 function HomePage(){
   return (
-    <div>
-    <Outlet/>
-  </div>
+    <Layout>
+      <div className="content">
+        <div className="nav">
+          <Layouts/>
+        </div>
+        <div className="main">
+          <Outlet/>
+        </div>
+      </div>
+    </Layout>
   )
 }
 export default HomePage;
---
+++ react-hooks/src/pages/home/layout.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+const items: MenuItem[] = [
+  {
+    key: 'sub2',
+    label: 'Navigation 2',
+    icon: <SettingOutlined />,
+    children: [
+      { key: '1', label: 'Option 9' },
+      { key: '2', label: 'Option 10' },
+      { key: '3', label: 'Option 11' },
+      { key: '4', label: 'Option 12' },
+    ],
+  },
+  {
+    key: 'sub4',
+    label: 'Navigation Three',
+    icon: <SettingOutlined />,
+    children: [
+      { key: '9', label: 'Option 9' },
+      { key: '10', label: 'Option 10' },
+      { key: '11', label: 'Option 11' },
+      { key: '12', label: 'Option 12' },
+    ],
+  }
+];
+
+const Layouts: React.FC = () => {
+  const onClick: MenuProps['onClick'] = (e) => {
+    console.log('click ', e);
+  };
+
+  return (
+    <Menu
+      onClick={onClick}
+      style={{ width: 256 }}
+      defaultSelectedKeys={['1']}
+      defaultOpenKeys={['sub1']}
+      mode="inline"
+      items={items}
+    />
+  );
+};
+
+export default Layouts;
