b047dc67ec7cd9035611e9464e61f746d854aa9f
chuxin <15115359055@139.com>
2025-04-28 17:54:47 +0800

    refactor(store/user): 简化用户存储逻辑并移除未使用的计算属性
    移除 `userNameLength` 计算属性，并简化 `updateUser` 方法中的类型检查逻辑，以提高代码的可维护性。同时，在 `App.vue` 中添加输入框以支持用户名称的编辑。

--- vue-pinia/src/App.vue
+++ vue-pinia/src/App.vue
@@ -7,12 +7,12 @@ const userStore = useUserStore()
 <template>
   <div>
     <h1>pinia 学习</h1>
-    
-    <!-- 用户user.ts -->
-     <h2>用户：user</h2>
-     <p>name: {{userStore.user.name}}</p>
-     <p>age: {{userStore.user.age}}</p>
 
+    <!-- 用户user.ts -->
+    <h2>用户：user</h2>
+    <p>name: {{ userStore.user.name }}</p>
+    <input type="text" v-model="userStore.user.name">
+    <p>age: {{ userStore.user.age }}</p>
 
 
   </div>
