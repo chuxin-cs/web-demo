720c6d597078585c0595023dff85df783762ff69
chuxin <15115359055@139.com>
2025-04-28 14:16:10 +0800

    feat: 添加路由配置和布局组件
    引入react-router-dom库，创建路由配置文件和布局组件，重构App.tsx以使用新路由系统。新增路由逻辑支持动态加载页面组件，提升应用的可扩展性和可维护性。

--- react-api/src/App.tsx
+++ react-api/src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
+import Router from "@/router"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <button onClick={ ()=> setCount(count+1) }>{{count}}</button>
-      <button onClick={ ()=> setCount((value)=> value+1) }></button>
+      <Router />
     </>
   )
 }
---
+++ react-api/src/router/routes.tsx
@@ -0,0 +1,30 @@
+import LayoutPage from "@/layout"
+const pages = import.meta.glob("./pages/**/*.tsx", { eager: true })
+
+// 用于存储子路由的数组
+export const childrenRoutes = [];
+
+// 遍历 pages 对象
+for (const path in pages) {
+  // 提取文件名，例如从 '../play/useLatest.tsx' 提取 'useLatest'
+  const fileName = path.split('/').pop()?.replace('.tsx', '');
+  if (fileName) {
+    const PageComponent = pages[path]?.default;
+    childrenRoutes.push({
+      path: fileName,
+      element: <PageComponent />
+    });
+  }
+}
+
+export const getRoutes = () => {
+  return childrenRoutes;
+}
+
+export const routes = [
+  {
+    path: "/",
+    element: <LayoutPage />,
+    children: childrenRoutes
+  }
+]
