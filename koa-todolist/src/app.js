c09dc449ee0603106c6291250201bdaf4462a2a8
chuxin <15115359055@139.com>
2025-05-08 16:59:18 +0800

    feat: 添加静态资源支持和跨域处理
    在应用中添加了静态资源目录支持，并启用了跨域处理，以便前端可以访问静态资源并解决跨域问题。同时，增加了对请求体的解析功能。

--- koa-todolist/src/app.js
+++ koa-todolist/src/app.js
@@ -1,10 +1,18 @@
 const Koa = require('koa');
+const path = require('path');
+const KoaStatic = require('koa-static');
 const bodyParser = require('koa-bodyparser');
-
+const cors = require('@koa/cors');
 
 function createApp() {
+  // 实例化koa
   const app = new Koa();
-  // 中间件
+  console.log(path.join(__dirname, '../static'),"path.join(__dirname, '../static')")
+  // 设置静态资源目录
+  app.use(KoaStatic(path.join(__dirname, '../static')));
+  // 跨域
+  app.use(cors());
+  // 解析请求体
   app.use(bodyParser());
   // 返回实例
   return app;
