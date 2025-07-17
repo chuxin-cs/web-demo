b3ece2346b0b50402929dab830f3a2674856bd83
chuxin <15115359055@139.com>
2025-07-17 14:35:05 +0800

    refactor(main.js): 改进项目启动命令的执行方式
    使用 exec 的 cwd 选项替代 cd 命令切换目录，并添加实时日志输出功能

--- demo-project/main.js
+++ demo-project/main.js
@@ -7,18 +7,25 @@ function startProject(projectName) {
   console.log(`正在启动项目: ${projectName}`);
   console.log(`项目路径: ${project.path}`);
   console.log(`执行命令: ${project.command}`);
-  
-  exec(`cd ${project.path} && ${project.command}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`项目 ${projectName} 启动失败:`, error);
-      return;
-    }
-    if (stderr) {
-      console.error(`项目 ${projectName} 执行命令时出现错误:`, stderr);
-      return;
-    }
-    console.log(`项目 ${projectName} 命令执行成功，输出:`, stdout);
+
+  const process = exec(project.command, { cwd: project.path });
+
+  process.stdout.on('data', (data) => {
+    console.log(`[${project.path}] stdout: ${data}`);
+    // 发送日志到渲染进程，更新UI
   });
+  
+  // exec(`cd ${project.path} && ${project.command}`, (error, stdout, stderr) => {
+  //   if (error) {
+  //     console.error(`项目 ${projectName} 启动失败:`, error);
+  //     return;
+  //   }
+  //   if (stderr) {
+  //     console.error(`项目 ${projectName} 执行命令时出现错误:`, stderr);
+  //     return;
+  //   }
+  //   console.log(`项目 ${projectName} 命令执行成功，输出:`, stdout);
+  // });
 }
 startProject('projectA')
 startProject('projectB')
