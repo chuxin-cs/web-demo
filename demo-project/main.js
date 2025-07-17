4daf968c0ea21a6d78e23ab0651b3786f57a24d1
chuxin <15115359055@139.com>
2025-07-17 14:18:36 +0800

    refactor: 将项目从 CommonJS 迁移至 ES 模块
    - 将 mian.js 重命名为 main.js 并使用 ES 模块语法
    - 更新 package.json 添加 "type": "module" 配置
    - 修改启动脚本指向新的 main.js 文件

---
+++ demo-project/main.js
@@ -0,0 +1,4 @@
+import { exec } from 'child_process';
+import projects from './file.json';
+
+console.log(projects)
--- demo-project/mian.js
+++
@@ -1,4 +0,0 @@
-const { exec } = require('child_process');
-const projects = require('./file.json');
-
-console.log(projects)
